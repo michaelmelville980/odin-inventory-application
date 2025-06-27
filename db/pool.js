08b827daffba2143cbe283e6aa3085361eb5287b
michael1303 <88162384+michael1303@users.noreply.github.com>
2025-06-27 11:19:19 -0400

    first commit

---
+++ db/pool.js
@@ -0,0 +1,10 @@
+
+const { Pool } = require("pg");
+
+module.exports = new Pool({
+  host:     process.env.PGHOST,
+  user:     process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port:     process.env.PGPORT,
+});
